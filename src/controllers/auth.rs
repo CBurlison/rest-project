8bc3fb7bd939e0944e90e2ea5aff388d83108f08
Cory Burlison <coryburlison@gmail.com>
2025-08-18 11:35:41 -0400

    Backup changes. Foreach still work in progress.

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,4 +1,6 @@
-use std::{str::FromStr};
+use std::{
+    cell::{ RefMut, RefCell }, rc::Rc
+};
 use actix_web::{
     self, HttpRequest, HttpResponse, Responder
 };
@@ -9,7 +11,6 @@ use serde::{
 };
 use serde_json::Value;
 use html5ever::{
-    interface::TreeSink, 
     parse_document, 
     tendril::{
         TendrilSink,
@@ -26,7 +27,8 @@ use rcdom::{
     Handle, 
     NodeData, 
     RcDom,
-    SerializableHandle
+    SerializableHandle,
+    Node
 };
 
 pub async fn hello() -> impl Responder {
@@ -37,6 +39,18 @@ pub async fn hello() -> impl Responder {
         <h1 class="foo">Hello, <i>world!</i></h1>
         <h2>
             <html-modal type="value" value="name" />
+            <html-modal type="if" value="test_true"></br>
+                test_true is displaying properly.
+            </html-modal>
+            <html-modal type="if" value="test_false"></br>
+                test_false id displaying. You messed up.
+            </html-modal>
+            <ul>
+            <html-modal type="foreach" value="test_data">
+                <li><html-modal type="foreach-value" value="" /></li>
+            </html-modal>
+            </ul>
+            <br/>Bottom text.
         </h2>
     "#.to_string();
 
@@ -47,7 +61,8 @@ pub async fn hello() -> impl Responder {
         ip: "127.0.0.1".to_string(),
         session: "this is the session ID".to_string(),
         test_data: vec!["test_val_1".to_string(), "test_val_2".to_string(), "test_val_3".to_string()],
-        test_bool: true
+        test_true: true,
+        test_false: false
     };
     
     let opts = ParseOpts {
@@ -57,12 +72,12 @@ pub async fn hello() -> impl Responder {
             },
         ..Default::default()
     };
-    let dom = parse_document(RcDom::default(), opts)
+    let mut dom = parse_document(RcDom::default(), opts)
         .from_utf8()
         .read_from(&mut html.as_bytes())
         .unwrap();
     
-    process_document(&dom, &user);
+    process_document(&mut dom, &user);
     
     let mut bytes = vec![];
     let document_clone: SerializableHandle = dom.document.clone().into();
@@ -73,7 +88,7 @@ pub async fn hello() -> impl Responder {
 }
 
 pub fn process_document<T>(
-    dom: &RcDom,
+    dom: &mut RcDom,
     modal: &T)
     where
         T: serde::ser::Serialize
@@ -81,13 +96,14 @@ pub fn process_document<T>(
         let json_value: Value = serde_json::to_value(&modal).unwrap();
         let document = &dom.document;
 
-        inner_process_document(&dom, 0, document, &json_value, &Value::Null);
+        inner_process_document(&dom, 0, document, None, &json_value, &Value::Null);
     }
 
 fn inner_process_document(
     dom: &RcDom, 
     indent: usize, 
-    handle: &Handle, 
+    handle: &Handle,
+    parent_children: Option<&mut RefMut<'_, Vec<Rc<Node>>>>,
     modal: &Value, 
     foreach_value: &Value)
     {
@@ -97,39 +113,43 @@ fn inner_process_document(
             ref attrs,
             ..
         } => {
-            match name.local.to_string().as_str() {
-                "html-modal" => {
-                    assert!(name.ns == ns!(html));
+            if name.local.to_string().as_str() == "html-modal" {
+                assert!(name.ns == ns!(html));
 
-                    // let mut attr_key = String::new();
-                    let mut attr_val = String::new();
+                let attr_info = get_attr_info(attrs);
 
-                    for attr in attrs.borrow().iter() {
-                        assert!(attr.name.ns == ns!());
-                        
-                        match attr.name.local.to_string().to_lowercase().as_str() {
-                            "type" => {
-                                // attr_key = attr.value.to_string();
-                            }
-                            "value" => {
-                                attr_val = attr.value.to_string();
-                            }
-                            _ => {}
+                let unwrapped_children = parent_children.unwrap();
+
+                match attr_info.name.to_lowercase().as_str() {
+                    "value" => {
+                        match_value(handle, modal, &attr_info.value, unwrapped_children);
+                    }
+                    "foreach-value" => {
+                        match_foreach_value(handle, foreach_value, &attr_info.value, unwrapped_children);
+                    }
+                    "if" => {
+                        match_if(handle, modal, &attr_info.value, unwrapped_children);
+                    }
+                    "foreach-if" => {
+                        match_foreach_if(handle, foreach_value, &attr_info.value, unwrapped_children);
+                    }
+                    "foreach" => {
+                        let val_split = attr_info.value.split(".");
+                        let mut disp_val = modal;
+                        for val in val_split {
+                            disp_val = &disp_val[val];
                         }
+                        
+                        for val in disp_val.as_array().unwrap() {
+                            let mut children = handle.children.borrow_mut();
+                            for child in children.clone().iter() {
+                                inner_process_document(dom, indent + 4, child, Some(&mut children), modal, val);
+                            }
+                        }
+                        return;
                     }
-
-                    let val_split = attr_val.split(".");
-
-                    let mut disp_val = modal;
-                    for val in val_split {
-                            disp_val = &modal[val];
-                    }
-                    
-                    
-                    let ten = StrTendril::from_str(disp_val.as_str().unwrap()).unwrap();
-                    dom.append(handle, html5ever::tree_builder::AppendText(ten));
+                    _ => {}
                 }
-                _ => {}
             }
         },
 
@@ -138,8 +158,106 @@ fn inner_process_document(
         _ => {}
     }
     
-    for child in handle.children.borrow().iter() {
-        inner_process_document(dom, indent + 4, child, modal, foreach_value);
+    let mut children = handle.children.borrow_mut();
+    for child in children.clone().iter() {
+        inner_process_document(dom, indent + 4, child, Some(&mut children), modal, foreach_value);
+    }
+}
+
+fn get_attr_info(attrs: &RefCell<Vec<html5ever::Attribute>>) -> AttrInfo {
+    let mut attr_info: AttrInfo = AttrInfo { name: "".to_string(), value: "".to_string() };
+
+    for attr in attrs.borrow().iter() {
+        assert!(attr.name.ns == ns!());
+    
+        match attr.name.local.to_string().to_lowercase().as_str() {
+            "type" => {
+                attr_info.name = attr.value.to_string();
+            }
+            "value" => {
+                attr_info.value = attr.value.to_string();
+            }
+            _ => {}
+        }
+    }
+    attr_info
+}
+
+fn match_value(handle: &Rc<Node>, modal: &Value, attr_val: &String, unwrapped_children: &mut RefMut<'_, Vec<Rc<Node>>>) {
+    let val_split = attr_val.split(".");
+    let mut disp_val = modal;
+
+    for val in val_split {
+        disp_val = &disp_val[val];
+    }
+
+    if let Some(pos) = unwrapped_children.iter().position(|child| Rc::ptr_eq(child, handle)) {
+        let mut text = StrTendril::new();
+        let text_handle = text.try_push_bytes(disp_val.as_str().unwrap().as_bytes());
+
+        match text_handle {
+            Ok(_) => {
+                unwrapped_children.insert(pos, Node::new(NodeData::Text { contents: RefCell::new(text) }));
+            }
+            Err(_) => {}
+        }
+    }
+}
+
+fn match_foreach_value(handle: &Rc<Node>, foreach_value: &Value, attr_val: &String, unwrapped_children: &mut RefMut<'_, Vec<Rc<Node>>>) {
+    let val_split = attr_val.split(".");
+    let mut disp_val = foreach_value;
+    println!("foreach in: {}", disp_val.to_string());
+
+    for val in val_split {
+        if disp_val.is_string() {
+            break;
+        }
+        
+        disp_val = &disp_val[val];
+    }
+    println!("foreach out: {}", disp_val.to_string());
+
+    if let Some(pos) = unwrapped_children.iter().position(|child| Rc::ptr_eq(child, handle)) {
+        let mut text = StrTendril::new();
+        let text_handle = text.try_push_bytes(disp_val.as_str().unwrap().as_bytes());
+
+        match text_handle {
+            Ok(_) => {
+                unwrapped_children.insert(pos, Node::new(NodeData::Text { contents: RefCell::new(text) }));
+            }
+            Err(_) => {}
+        }
+    }
+}
+
+fn match_if(handle: &Rc<Node>, modal: &Value, attr_val: &String, unwrapped_children: &mut RefMut<'_, Vec<Rc<Node>>>) {
+    let val_split = attr_val.split(".");
+    let mut disp_val = modal;
+                        
+    for val in val_split {
+        disp_val = &disp_val[val];
+    }
+                        
+    if !disp_val.as_bool().unwrap() {
+        if let Some(pos) = unwrapped_children.iter().position(|child| Rc::ptr_eq(child, handle)) {
+            unwrapped_children.remove(pos);
+        }
+    }
+}
+
+fn match_foreach_if(handle: &Rc<Node>, foreach_value: &Value, attr_val: &String, unwrapped_children: &mut RefMut<'_, Vec<Rc<Node>>>) {
+    let val_split = attr_val.split(".");
+    let mut disp_val = foreach_value;
+
+    for val in val_split {
+        disp_val = &disp_val[val];
+    }
+
+    if !disp_val.as_bool().unwrap() {
+        if let Some(pos) = unwrapped_children.iter().position(|child| Rc::ptr_eq(child, handle)) {
+            unwrapped_children.remove(pos);
+        }
     }
 }
 
@@ -164,5 +282,11 @@ struct User {
     ip: String,
     session: String,
     test_data: Vec<String>,
-    test_bool: bool
+    test_true: bool,
+    test_false: bool
+}
+
+struct AttrInfo {
+    name: String,
+    value: String
 }
